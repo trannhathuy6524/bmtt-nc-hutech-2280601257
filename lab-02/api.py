04e364dc285ef13cc097ca98bb920aedcba2cc3e
Tran Nhat Huy <trannhathuy6524@gmail.com>
2025-03-04 11:10:16 +0700

    [add] lab-02 vigenere

--- lab-02/api.py
+++ lab-02/api.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from cipher.caesar import CaesarCipher
+from cipher.vigenere import VigenereCipher
 app = Flask(__name__)
 
 caesar_cipher = CaesarCipher()
@@ -19,6 +20,24 @@ def caesar_decrypt():
     key = int(data['key'])
     plain_text = caesar_cipher.decrypt_text(encrypted_text, key)
     return jsonify({'plain_text': plain_text})
+
+
+vigenere_cipher = VigenereCipher()
+@app.route("/api/vigenere/encrypt", methods=["POST"])
+def vigenere_encrypt():
+    data = request.json
+    plain_text = data['plain_text']
+    key = data['key']
+    encrypted_text = vigenere_cipher.vigenere_encrypt(plain_text, key)
+    return jsonify({'encrypted_text': encrypted_text})
+@app.route("/api/vigenere/decrypt", methods=["POST"])
+def vigenere_decrypt():
+    data = request.json
+    encrypted_text = data['encrypted_text']
+    key = data['key']
+    plain_text = vigenere_cipher.vigenere_decrypt(encrypted_text, key)
+    return jsonify({'plain_text': plain_text})
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
